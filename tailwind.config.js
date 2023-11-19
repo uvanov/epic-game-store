7a2430068a7c87f2af1efc6251cdb5851a97088f
uvanov <kirill.ivanov000@gmail.com>
2023-11-19 13:36:00 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#121212',
+        gray: '#A4A4A4',
+        dark: {
+          100: '#202020',
+          200: '#363636'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
